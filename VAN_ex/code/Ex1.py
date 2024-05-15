bce995b6685f4273bf33b6a8b291a08e696a7aa8
AmitaiOvadia <amitaiovadia@gmail.com>
2024-05-15 15:36:20 +0300

    1405 try to push main.py

--- VAN_ex/code/Ex1.py
+++ VAN_ex/code/Ex1.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
+# Constants
 NUMBER_OF_FEATURES = 128
 NUMBER_OF_FEATURES_TO_SHOW = 500
 RATIO = 0.4
@@ -10,23 +11,64 @@ DATA_PATH = r'C:\Users\amita\OneDrive\Desktop\master\year 2\SLAM\SLAMProject\VAN
 
 
 def read_images(idx):
+    """
+    Reads a pair of images from the dataset.
+
+    Parameters:
+    idx (int): Index of the image pair.
+
+    Returns:
+    tuple: A tuple containing the left and right images.
+    """
     img_name = '{:06d}.png'.format(idx)
-    img1 = cv2.imread(DATA_PATH + '\image_0\\' + img_name, 0)
-    img2 = cv2.imread(DATA_PATH + '\image_1\\' + img_name, 0)
+    img1 = cv2.imread(DATA_PATH + '\\image_0\\' + img_name, 0)
+    img2 = cv2.imread(DATA_PATH + '\\image_1\\' + img_name, 0)
     return img1, img2
 
 
 def detect_features(image1, image2, feature_extractor):
+    """
+    Detects and computes features in two images.
+
+    Parameters:
+    image1 (ndarray): The first image.
+    image2 (ndarray): The second image.
+    feature_extractor (cv2.Feature2D): Feature extractor.
+
+    Returns:
+    tuple: Keypoints and descriptors for both images.
+    """
     kp1, des1 = feature_extractor.detectAndCompute(image1, None)
     kp2, des2 = feature_extractor.detectAndCompute(image2, None)
     return kp1, des1, kp2, des2
 
 
 def display(Left_img, right_img, keypoints1, keypoints2, good_matches=None,
-                                  bad_matches=None, gap=10, label_width=50,
-                                  number_of_features_to_show=NUMBER_OF_FEATURES_TO_SHOW,
-                                  show_matches=False, line_thickness=1,
-                                  title="", save_name=""):
+            bad_matches=None, gap=10, label_width=50,
+            number_of_features_to_show=NUMBER_OF_FEATURES_TO_SHOW,
+            show_matches=False, line_thickness=1,
+            title="", save_name=""):
+    """
+    Displays the images with keypoints or matches.
+
+    Parameters:
+    Left_img (ndarray): The left image.
+    right_img (ndarray): The right image.
+    keypoints1 (list): Keypoints of the left image.
+    keypoints2 (list): Keypoints of the right image.
+    good_matches (list): Good matches between keypoints.
+    bad_matches (list): Bad matches between keypoints.
+    gap (int): Gap between images.
+    label_width (int): Width of the label.
+    number_of_features_to_show (int): Number of features to show.
+    show_matches (bool): Flag to show matches.
+    line_thickness (int): Thickness of the lines.
+    title (str): Title of the plot.
+    save_name (str): the file saving name.
+
+    Returns:
+    None
+    """
     left_label = np.ones((Left_img.shape[0], label_width), dtype=np.uint8) * 255
     right_label = np.ones((right_img.shape[0], label_width), dtype=np.uint8) * 255
     Left_0_with_label = np.hstack((left_label, Left_img))
@@ -45,42 +87,34 @@ def display(Left_img, right_img, keypoints1, keypoints2, good_matches=None,
     combined_image = np.vstack((Left_0_with_label, gap_array, Right_0_with_label))
     combined_image_color = cv2.cvtColor(combined_image, cv2.COLOR_GRAY2BGR)
 
-    # inds_to_show = np.random.randint(0, len(good_matches), size=number_of_features_to_show)
-    # good_matches = [good_matches[ind] for ind in inds_to_show]
-
     if show_matches:
-        # Draw good matches in green
         for match in good_matches[:number_of_features_to_show]:
             img1_idx = match.queryIdx
             img2_idx = match.trainIdx
             (x1, y1) = keypoints1[img1_idx].pt
             (x2, y2) = keypoints2[img2_idx].pt
-            color = (0, 255, 0)  # Green color for lines
+            color = (0, 255, 0)
             cv2.line(combined_image_color, (int(x1) + label_width, int(y1)),
                      (int(x2) + label_width, int(y2) + Left_0_with_label.shape[0] + gap), color, line_thickness)
 
-        # Draw bad matches in red
         if bad_matches:
             for match in bad_matches[:number_of_features_to_show]:
                 img1_idx = match.queryIdx
                 img2_idx = match.trainIdx
                 (x1, y1) = keypoints1[img1_idx].pt
                 (x2, y2) = keypoints2[img2_idx].pt
-                color = (255, 0, 0)  # Red color for lines
+                color = (255, 0, 0)
                 cv2.line(combined_image_color, (int(x1) + label_width, int(y1)),
                          (int(x2) + label_width, int(y2) + Left_0_with_label.shape[0] + gap), color, line_thickness)
     else:
-        # Draw keypoints on the left image
         for kp in keypoints1[:number_of_features_to_show]:
             x, y = kp.pt
-            cv2.circle(combined_image_color, (int(x) + label_width, int(y)), 3, (0, 255, 0), -1)  # Green circles
+            cv2.circle(combined_image_color, (int(x) + label_width, int(y)), 3, (0, 255, 0), -1)
 
-        # Draw keypoints on the right image
         for kp in keypoints2[:number_of_features_to_show]:
             x, y = kp.pt
-            cv2.circle(combined_image_color, (int(x) + label_width, int(y) + Left_0_with_label.shape[0] + gap), 3, (0, 255, 0), -1)  # Green circles
+            cv2.circle(combined_image_color, (int(x) + label_width, int(y) + Left_0_with_label.shape[0] + gap), 3, (0, 255, 0), -1)
 
-    # Save the image using matplotlib
     plt.figure(figsize=(12, 8))
     plt.imshow(combined_image_color)
     if show_matches:
@@ -93,6 +127,17 @@ def display(Left_img, right_img, keypoints1, keypoints2, good_matches=None,
 
 
 def find_closest_features(des1, des2, distance_func=cv2.NORM_L2):
+    """
+    Finds the closest features between two sets of descriptors.
+
+    Parameters:
+    des1 (ndarray): Descriptors of the first image.
+    des2 (ndarray): Descriptors of the second image.
+    distance_func (int): Distance function to use.
+
+    Returns:
+    list: Sorted matches between descriptors.
+    """
     bf = cv2.BFMatcher(distance_func, crossCheck=True)
     matches = bf.match(des1, des2)
     matches = sorted(matches, key=lambda x: x.distance)
@@ -100,6 +145,17 @@ def find_closest_features(des1, des2, distance_func=cv2.NORM_L2):
 
 
 def reject_matches_using_significance_test(des1, des2, ratio=RATIO):
+    """
+    Rejects matches using a significance test based on distance ratios.
+
+    Parameters:
+    des1 (ndarray): Descriptors of the first image.
+    des2 (ndarray): Descriptors of the second image.
+    ratio (float): Ratio threshold for the significance test.
+
+    Returns:
+    tuple: Good matches, good ratios, bad matches, and bad ratios.
+    """
     bf = cv2.BFMatcher(cv2.NORM_L2)
     matches = bf.knnMatch(des1, des2, k=2)
     good_matches = []
@@ -115,7 +171,6 @@ def reject_matches_using_significance_test(des1, des2, ratio=RATIO):
             bad_matches.append(first_match)
             bad_ratios.append(matches_ratio)
     return good_matches, good_ratios, bad_matches, bad_ratios
-
 
 def Q11():
     image1, image2 = read_images(0)
