26ddc83956083fc77c687fe93b5c23bd680b0459
AmitaiOvadia <101287679+AmitaiOvadia@users.noreply.github.com>
2024-07-29 08:54:34 +0300

    Add files via upload

--- VAN_ex/code/ex4/Ex4.py
+++ VAN_ex/code/ex4/Ex4.py
@@ -5,14 +5,14 @@ import cv2
 import pickle
 from typing import List, Tuple, Dict, Sequence, Optional
 from timeit import default_timer as timer
+from tqdm import tqdm
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-import networkx as nx
+# import networkx as nx
 
 import random
 from utils import utils
 from utils.visualize import Visualizer
 from utils.tracking_database import TrackingDB
-
 # Add the path to the VAN_ex directory to the system path
 from ex3.Ex3 import ImageProcessor
 import matplotlib.pyplot as plt
@@ -20,21 +20,26 @@ import matplotlib.pyplot as plt
 
 
 AKAZE = 'AKAZE'
-SAVE_NAME = "tracking_db"
+SAVE_NAME = "tracking_db_2_acc_y_2_4e-4_blur_1_it_5_akaze"
+RANSAC_ACCURACY = 2
+Y_DIST_TOLERATION = 2
+MIN_RANSAC_ITERATIONS = 5
 
 
 # Initialize ImageProcessor
 def initialize_image_processor(feature_extractor_name=AKAZE,
-                               y_dist_threshold=1,
-                               accuracy=1.5,
-                               min_ransac_iterations=5):
-    return ImageProcessor(feature_extractor_name=feature_extractor_name, y_dist_threshold=y_dist_threshold,
-                          accuracy=accuracy, min_ransac_iterations=min_ransac_iterations)
+                               y_dist_threshold=Y_DIST_TOLERATION,
+                               accuracy=RANSAC_ACCURACY,
+                               min_ransac_iterations=MIN_RANSAC_ITERATIONS):
+    return ImageProcessor(feature_extractor_name=feature_extractor_name,
+                          y_dist_threshold=y_dist_threshold,
+                          accuracy=accuracy,
+                          min_ransac_iterations=min_ransac_iterations)
 
 
 # Initialize TrackingDB
-def initialize_tracking_db():
-    return TrackingDB()
+def initialize_tracking_db(K, M1, M2):
+    return TrackingDB(K, M1, M2)
 
 
 def create_matches_list(descriptors_frame_current, descriptors_frame_prev):
@@ -53,19 +58,19 @@ def create_matches_list(descriptors_frame_current, descriptors_frame_prev):
 def fill_tracking_db():
     directory_path = os.path.join(utils.DATA_PATH, "image_0")
     num_frames = sum(os.path.isfile(os.path.join(directory_path, f)) for f in os.listdir(directory_path))
-    # num_frames = 100  # Set the number of frames you want to process
+    # num_frames = 30 # Set the number of frames you want to process
 
     # Initialize ImageProcessor and TrackingDB
     processor = initialize_image_processor()
-    tracking_db = initialize_tracking_db()
+    tracking_db = initialize_tracking_db(processor.K, processor.M1, processor.M2)
 
     left_descriptors_frames = []
     right_descriptors_frames = []
     matches_dics_frames = []
     left_key_points_frames = []
     right_key_points_frames = []
-    for frame_idx in range(num_frames):
-        print(frame_idx)
+    for frame_idx in tqdm(range(num_frames)):
+        # print(frame_idx)
         left, right = utils.read_images(frame_idx)
         (cur_left_descriptors, cur_right_descriptors, left_key_points,
          matches_dict, right_key_points, matches_init) = processor.get_matches(left, right)
@@ -93,7 +98,8 @@ def fill_tracking_db():
                                                 matches_to_previous_left=None,
                                                 total_num_keypoints=None,
                                                 num_inliers=None,
-                                                inliers=None)
+                                                inliers=None,
+                                                left_extrinsic_cam=None)
             continue
 
         # find outliers using RANSAC now
@@ -112,7 +118,7 @@ def fill_tracking_db():
                                                                                            processor, right_0_inds,
                                                                                            right_1_inds,
                                                                                            right_key_points_frames,
-                                                                                           accuracy=1.5)
+                                                                                           accuracy=RANSAC_ACCURACY)
         # left_0_inliers = [left_0_inds[i] for i in pnp_inliers]
         # right_0_inliers = [right_0_inds[i] for i in pnp_inliers]
         left_1_inliers = [left_1_inds[i] for i in pnp_inliers]
@@ -135,14 +141,17 @@ def fill_tracking_db():
         prev_features = tracking_db.last_features()
         matches_to_previous_left = utils.find_closest_features(prev_features, features_left)
         inliers = remove_track_outliers(final_left1_extrinsic_mat, links, matches_to_previous_left, processor,
-                                        tracking_db, accuracy=1.5)
+                                        tracking_db, accuracy=RANSAC_ACCURACY)
         ratio = np.sum(inliers)/len(left_0_inds)
-        print(f"frame: inliers: {np.sum(inliers)}/{len(matches_to_previous_left)} ratio: {ratio}, num ransac iterations: {num_iterations}")
-        cur_frameId = tracking_db.add_frame(links=links, left_features=features_left, right_features=features_right,
+        # print(f"frame: inliers: {np.sum(inliers)}/{len(matches_to_previous_left)} ratio: {ratio}, num ransac iterations: {num_iterations}")
+        cur_frameId = tracking_db.add_frame(links=links,
+                                            left_features=features_left,
+                                            right_features=features_right,
                                             matches_to_previous_left=matches_to_previous_left,
                                             total_num_keypoints=len(left_1_inds),
                                             num_inliers=np.sum(inliers),
-                                            inliers=inliers)
+                                            inliers=inliers,
+                                            left_extrinsic_cam=final_left1_extrinsic_mat)
     tracking_db.serialize(SAVE_NAME)
 
 
@@ -280,118 +289,6 @@ def create_trivial_dict(n):
     return matches_dict
 
 
-def fill_tracking_db3():
-    directory_path = os.path.join(utils.DATA_PATH, "image_0")
-    num_frames = sum(os.path.isfile(os.path.join(directory_path, f)) for f in os.listdir(directory_path))
-    # num_frames = 100  # Set the number of frames you want to process
-
-    # Initialize ImageProcessor and TrackingDB
-    processor = initialize_image_processor()
-    tracking_db = initialize_tracking_db()
-
-    for frame_idx in range(num_frames):
-        print(frame_idx)
-        left, right = utils.read_images(frame_idx)
-        cur_left_descriptors, cur_right_descriptors, left_key_points, matches_dict, right_key_points, matches_init = processor.get_matches(
-            left, right)
-
-        # frame 0:
-        if frame_idx == 0:
-            filtered_left_features, filtered_right_features, links = (
-                         tracking_db.create_links(
-                         left_features=cur_left_descriptors,
-                         right_features=cur_right_descriptors,
-                         kp_left=left_key_points,
-                         kp_right=right_key_points,
-                         matches=matches_init,
-                         inliers=None))
-            cur_frameId = tracking_db.add_frame(links=links,
-                                                left_features=filtered_left_features,
-                                                right_features=filtered_right_features,
-                                                matches_to_previous_left=None,
-                                                inliers=None)
-            continue
-
-        # find outliers using RANSAC now
-        prev_left_descriptors = tracking_db.last_features()
-        prev_links = tracking_db.prev_frame_links
-        prev_matches_dict = create_trivial_dict(len(prev_links))
-        left_0_left_1_matches = utils.find_closest_features(prev_left_descriptors, cur_left_descriptors)
-
-        matches_dict_left0_left1 = processor.create_matches_dict(left_0_left_1_matches)
-        left_0_inds, right_0_inds, left_1_inds, right_1_inds = processor.get_points_in_4_images(
-            matches_dict_left0_left1,
-            prev_matches_dict,
-            matches_dict)
-
-        left_0_points_2D = []
-        right_0_points_2D = []
-        for lnk in prev_links:
-            x_left = lnk.x_left
-            x_right = lnk.x_right
-            y = lnk.y
-            left_0_points_2D.append([x_left, y])
-            right_0_points_2D.append([x_right, y])
-
-        left_0_points_2D = np.array(left_0_points_2D)[left_0_inds]
-        right_0_points_2D = np.array(right_0_points_2D)[right_0_inds]
-
-        left_1_points_2D = [left_key_points[i] for i in left_1_inds]
-        right_1_points_2D = [right_key_points[i] for i in right_1_inds]
-        left_1_points_2D, right_1_points_2D = utils.get_points_from_key_points(left_1_points_2D,
-                                                                               right_1_points_2D)
-
-        points_3D_0 = utils.triangulate_points(processor.P1, processor.P2, left_0_points_2D, right_0_points_2D)
-        final_left1_extrinsic_mat, pnp_inliers, num_iterations = processor.run_RANSAC(world_3D_points=points_3D_0,
-                                                                                      left0_2D_points=left_0_points_2D,
-                                                                                      right0_2D_points=right_0_points_2D,
-                                                                                      left1_2D_points=left_1_points_2D,
-                                                                                      right1_2D_points=right_1_points_2D,
-                                                                                      K=processor.K,
-                                                                                      M_L0=processor.M1,
-                                                                                      M_R0=processor.M2)
-
-        left_0_inliers = [left_0_inds[i] for i in pnp_inliers]
-        right_0_inliers = [right_0_inds[i] for i in pnp_inliers]
-        left_1_inliers = [left_1_inds[i] for i in pnp_inliers]
-        right_1_inliers = [right_1_inds[i] for i in pnp_inliers]
-
-        kp_left = [left_key_points[i] for i in left_1_inliers]
-        kp_right = [right_key_points[i] for i in right_1_inliers]
-        features_left = cur_left_descriptors[left_1_inliers]
-        features_right = cur_right_descriptors[right_1_inliers]
-        matches_right_left = []
-        for i in range(len(features_left)):
-            desc0 = features_left[i]
-            desc1 = features_right[i]
-            distance = np.linalg.norm(desc1 - desc0)
-            match = cv2.DMatch(_queryIdx=i, _trainIdx=i, _imgIdx=0, _distance=distance)
-            matches_right_left.append(match)
-        _, _, links = tracking_db.create_links(left_features=features_left,
-                                               right_features=features_right,
-                                                   kp_left=kp_left,
-                                                   kp_right=kp_right,
-                                                   matches=matches_right_left,
-                                                   inliers=None)
-
-        prev_features = tracking_db.last_features()
-        prev_features_inliers = prev_features[left_0_inliers]
-        # matches_to_previous_left = utils.find_closest_features(prev_features, features_left)
-        matches_to_previous_left = []
-        num_matches = len(left_0_inliers)
-        for i in range(num_matches):
-            desc0 = prev_features_inliers[i]
-            desc1 = features_left[i]
-            distance = np.linalg.norm(desc1 - desc0)
-            match = cv2.DMatch(_queryIdx=left_0_inliers[i], _trainIdx=i, _imgIdx=0, _distance=distance)
-            matches_to_previous_left.append(match)
-
-        cur_frameId = tracking_db.add_frame(links=links, left_features=features_left, right_features=features_right,
-                                            matches_to_previous_left=matches_to_previous_left,
-                                            inliers=None)
-    tracking_db.serialize(SAVE_NAME)
-
-
 def connectivity_graph(tracking_db):
     all_frames = tracking_db.all_frames()
     outgoing_tracks_counts = []
@@ -483,19 +380,7 @@ def present_track(frame_ind_for_3D, trackId, tracking_db):
     # Define the translation vector (0.53 meters shift in the x-axis)
     left_cameras = np.array(processor.load_all_ground_truth_camera_matrices(processor.GROUND_TRUTH_PATH))[frames]
     # Initialize the right_cameras array
-    right_cameras = np.zeros_like(left_cameras)
-    # left_cameras = np.array([processor.M1])
-    # Calculate the right camera matrices
-    for i in range(len(left_cameras)):
-        R = left_cameras[i, :, :3]  # Extract the rotation matrix
-        t = left_cameras[i, :, 3]  # Extract the translation vector
-
-        # Apply the translation in the camera coordinate system
-        new_t = t + translation_vector
-
-        # Construct the right camera matrix
-        right_cameras[i, :, :3] = R
-        right_cameras[i, :, 3] = new_t
+    right_cameras = create_right_cameras_Rt(left_cameras, translation_vector)
     # all 2D tracks
     left_track_2D = []
     right_track_2D = []
@@ -543,6 +428,23 @@ def present_track(frame_ind_for_3D, trackId, tracking_db):
     plt.savefig(f"reprojection errors along track, {trianglated_frame} frame.png")
 
 
+def create_right_cameras_Rt(left_cameras, translation_vector):
+    right_cameras = np.zeros_like(left_cameras)
+    # left_cameras = np.array([processor.M1])
+    # Calculate the right camera matrices
+    for i in range(len(left_cameras)):
+        R = left_cameras[i, :, :3]  # Extract the rotation matrix
+        t = left_cameras[i, :, 3]  # Extract the translation vector
+
+        # Apply the translation in the camera coordinate system
+        new_t = t + translation_vector
+
+        # Construct the right camera matrix
+        right_cameras[i, :, :3] = R
+        right_cameras[i, :, 3] = new_t
+    return right_cameras
+
+
 def Q42(tracking_db):
     tracking_db.present_tracking_statistics()
 
@@ -569,11 +471,29 @@ def Q47(tracking_db):
                                     min_length=min_length)
 
 
+def show_camera_centers(tracking_db, processor):
+
+    camera_centers_dict = tracking_db.frameId_to_camera_center[0]
+    num_frames = len(camera_centers_dict)
+
+    true_Rt = processor.load_all_ground_truth_camera_matrices(processor.GROUND_TRUTH_PATH)[:num_frames]
+    ground_truth_centers = [utils.get_camera_center_from_Rt(Rt) for Rt in true_Rt]
+
+    all_centers = []
+    for i in range(num_frames):
+        center_i = camera_centers_dict[i]
+        all_centers.append(center_i)
+    all_centers, ground_truth_centers =np.array(all_centers), np.array(ground_truth_centers)
+    Visualizer.plot_trajectories_plt(all_centers, ground_truth_centers)
+
 
 if __name__ == '__main__':
     fill_tracking_db()
-    tracking_db = TrackingDB()
+    processor = initialize_image_processor()
+    tracking_db = TrackingDB(processor.K, processor.M1, processor.M2)
     tracking_db.load(SAVE_NAME)
+    show_camera_centers(tracking_db, processor)
+
     Q42(tracking_db)
     Q43(tracking_db)
     Q44(tracking_db)
