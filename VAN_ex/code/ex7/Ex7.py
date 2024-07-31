09d3f34f72afcd6070f5c15f0db07c92ae132072
AmitaiOvadia <101287679+AmitaiOvadia@users.noreply.github.com>
2024-07-31 19:49:51 +0300

    almost finished the last graph not good

--- VAN_ex/code/ex7/Ex7.py
+++ VAN_ex/code/ex7/Ex7.py
@@ -4,6 +4,7 @@ import sys
 import os
 import matplotlib.pyplot as plt
 import matplotlib
+import gtsam
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils.BundleAdjusment import BundleAdjusment, Bundelon
 from utils.PoseGraph import PoseGraph
@@ -53,8 +54,95 @@ def task_7_1(tracking_db, processor):
     bundle_object.create_and_solve_all_bundle_windows()
     plot_camera_centers_with_labels(bundle_object)
     pose_graph = PoseGraph(bundle_object, processor, do_loop_closure=True)
-    a=0
+    all_optimized_values = pose_graph.loop_closure_pair_to_optimized_values
+    all_loop_closure_frames = [all_optimized_values[i][1] for i in range(len(all_optimized_values))]
 
+    # How many successful loop closures were detected?
+    print(f"the total number of loop closure detected was: {pose_graph.total_num_loop_closures} "
+          f"across {pose_graph.loop_closure_frames_counter} loop closure frames (frames from which at least one loop closure was detected)")
+
+    # Plot the match results of a single successful consensus match of your choice. (For the left
+    # images, inliers and outliers in different color)
+    # *** done inside the PoseGraph class in the function Visualizer.display_key_points_with_inliers_outliers inside
+    # get_initial_pose_estimate_and_links ***
+
+    # Choose 4 versions of the pose graph along the process and plot them (including location
+    # covariance).
+    plot_locations_with_covariance_gtsam(all_loop_closure_frames, all_optimized_values, pose_graph.ground_truth_locations)
+
+    # Plot the pose graph locations along with the ground truth both with and without loop
+    # closures.
+    # *** done during the pose graph optimization in the function:
+    # self.save_cur_camera_locations_status_vs_ground_truth() ***
+
+    # Plot a graph of the absolute location error for the whole pose graph both with and without
+    # loop closures.
+    absolute_location_error_before_vs_after_loop_closures(all_optimized_values, pose_graph)
+
+    # Plot a graph of the location uncertainty size for the whole pose graph both with and
+    # without loop closures. (What measure of uncertainty size did you choose?)
+    plot_location_uncertainty(pose_graph)
+
+
+def plot_location_uncertainty(pose_graph):
+    plt.figure()
+    initial_uncertainty_sizes = pose_graph.initial_location_uncertainty_per_frame
+    final_uncertainty_sizes = pose_graph.final_location_uncertainty_per_frame
+    plt.plot(initial_uncertainty_sizes, label="Initial uncertainty", color="red")
+    plt.plot(final_uncertainty_sizes, label="final uncertainty", color="blue")
+    plt.yscale('log')
+    plt.xlabel('key frame')
+    plt.ylabel('Uncertainty Size (log scale)')
+    plt.title('Uncertainty Sizes on Logarithmic Scale')
+    plt.legend()
+    plt.savefig("Uncertainty sizes before and after loop closure.png", dpi=600)
+    plt.close()
+
+
+def absolute_location_error_before_vs_after_loop_closures(all_optimized_values, pose_graph):
+    ground_truth_key_frames = pose_graph.ground_truth_locations[pose_graph.key_frames]
+    all_camera_centers_initial = all_optimized_values[0][-1]
+    all_camera_centers_final = all_optimized_values[len(all_optimized_values) - 1][-1]
+    l2_distance_before_loop_closure = np.linalg.norm(all_camera_centers_initial - ground_truth_key_frames, axis=-1)
+    l2_distance_after_loop_closure = np.linalg.norm(all_camera_centers_final - ground_truth_key_frames, axis=-1)
+    plt.figure()
+    plt.plot(l2_distance_before_loop_closure, label="L2 distance before loop closure", color="red")
+    plt.plot(l2_distance_after_loop_closure, label="L2 distance after loop closure", color="blue")
+    plt.legend()
+    plt.title("L2 Distance Before and after loop closure")
+    plt.savefig("L2_distance_before_vs_after_loop_closure.png", dpi=600)
+
+
+def plot_locations_with_covariance_gtsam(all_loop_closure_frames, all_optimized_values, ground_truth_locations):
+    # Calculate limits
+    margin = 50
+    xmin = ground_truth_locations[:, 0].min() - margin
+    xmax = ground_truth_locations[:, 0].max() + margin
+    zmin = ground_truth_locations[:, 2].min() - margin
+    zmax = ground_truth_locations[:, 2].max() + margin
+    start = 0
+    finish = len(all_loop_closure_frames) - 1
+    first_loop_closure = 1  # first loop closure
+    half_way = int((start + finish) // 2)  # middle
+    for i, loop_closure_ind in enumerate([start, first_loop_closure, half_way, finish]):
+        print(i)
+        optimized_values = all_optimized_values[loop_closure_ind][0]
+        marginals = all_optimized_values[loop_closure_ind][2]
+        plt.close()
+        fig = plt.figure()
+        gtsam.utils.plot.plot_trajectory(0, optimized_values, marginals=marginals, scale=1)
+        plt.tight_layout()
+
+        ax = plt.gca()
+        # Set axis limits
+        ax.set_xlim([xmin, xmax])
+        ax.set_ylim([zmin, zmax])
+        ax.view_init(elev=0, azim=270)
+        # ax.set_aspect('equal')
+        plt.savefig(f"loop closure ind {loop_closure_ind}.png", dpi=600)
+        plt.close(fig)
+        plt.close()
+        del fig
 
 
 if "__main__" == __name__:
